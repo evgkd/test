14885856db39808fac771b2c0781c2a25e43ea85
evgkd <evgkd1993@gmail.com>
2025-06-18 14:54:34 +0300

    update

---
+++ test.js
@@ -0,0 +1,7 @@
+const name = "Timeweb";
+const person = { first: name };
+console.log(person);
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+sayHelloLinting("Timeweb");
